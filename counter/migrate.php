de3a6af7dd02884e234bd65f8ecd6147424f9eea
Mitsuharu Uemoto <uemoto@ccs.tsukuba.ac.jp>
2018-05-24 17:56:32 +0900

    feat: support sakura internet server

--- counter/migrate.php
+++ counter/migrate.php
@@ -1,14 +1,31 @@
 <?php
 
 $DBFILE = "data/database.sqlite";
-$LOGDIR = "../../../www/log";
-$PATTERN = "|^([0-9\.]+).*?\[([0-9a-zA-Z/]+):([0-9:]+).*?GET /download/(.+?) .*? 200 .*$|";
+//$LOGDIR = "../../../www/log/*/*/";
+
+/*
+$LOGDIR = "..";
+$MODE = 0;
+$PATTERN = '/salmon-access\.log\.([0-9]+)/';
+$DATE1 = 20170614;
+$DATE2 = 20180514;
+*/
+
+$LOGDIR = "../../../log";
+$MODE = 1;
+$PATTERN = '/access_log_([0-9]+)\.gz/';
+$DATE1 = 20180515; // Start to switch the sakura internet server
+$DATE2 = 99999999; // End
+
+$BASEDIR = array(
+  "/download"
+);
 
 $IGNORE = array(
-  "example.bin",
-  "extract_session.php",
-  "salmon-v.0.0.1.tar.gz",
-  "SALMON-v0.0.1.tar.gz"
+  "/download/example.bin",
+  "/download/extract_session.php",
+  "/download/salmon-v.0.0.1.tar.gz",
+  "/download/SALMON-v0.0.1.tar.gz"
 );
 
 include_once("geoip-api-php/src/geoipcity.inc");
@@ -45,8 +62,27 @@ INSERT INTO DOWNLOAD (TITLE,IPADDR,XTIME,XDATE,XDATE_EN,COUNTRY,CITY)
 VALUES (:title,:ipaddr,:xtime,:xdate,:xdate_en,:country,:city);
 SQL;
 
+function purse_log($text, $mode, $basedir) {
+  if (preg_match("|^(.*?)\s+\[(.*?)\]\s+\"(.*?)\"\s+200|", $text, $matches)) {
+    $temp1 = explode(" ", $matches[1]);
+    $temp2 = explode(" ", $matches[2]);
+    $temp3 = explode(" ", $matches[3]);
+    $item = $temp3[1];
 
-
+    if (in_array(dirname($item), $basedir)) {
+      preg_match("|^(.*?):([\d:]+)$|", $temp2[0], $matches);
+      $xdate = $matches[1];
+      $xtime = $matches[2];
+      if ($mode == 0) {
+        $addr = $temp1[0];
+      } else if  ($mode == 1) {
+        $addr = gethostbyname($temp1[1]);
+      }
+      return array($addr, $xdate, $xtime, $item);
+    }
+  }
+  return array();
+}
 
 # データベースへ接続
 echo "Connecting to Datebase: $DBFILE\n";
@@ -62,22 +98,36 @@ $db->beginTransaction();
 
 # ログファイル一覧を取得
 echo "Scanning apache logs from $LOGDIR\n";
-$logfile_list = glob("$LOGDIR/*/*/salmon-access.log.*");
+$logfile_list = glob("$LOGDIR/*log*");
 $ncount_logfile = 0; # 読み込んだログ数カウント
 foreach ($logfile_list as $logfile) {
-    $mtime =  filemtime($logfile);
-    if (array_key_exists($logfile, $prevous_log) && ($prevous_log[$logfile][0] == $mtime)) {
-        continue;
-    }
 
-    $yyyymmdd = pathinfo($logfile, PATHINFO_EXTENSION);
+    $logfile_title = pathinfo($logfile, PATHINFO_BASENAME);
+    
+    if (! preg_match($PATTERN, $logfile_title, $matches)) {
+      continue;
+    }
+    
+    $yyyymmdd = $matches[1];
+    
+    if (($yyyymmdd < $DATE1) || ($DATE2 <= $yyyymmdd)) {
+      continue;
+    }
+    
+    $mtime =  filemtime($logfile);
+    if (array_key_exists($logfile_title, $prevous_log)) {
+      if ($prevous_log[$logfile_title][0] == $mtime) {
+        continue;
+      }
+    }
+    
     $yyyy = substr($yyyymmdd, 0, 4);
     $mm = substr($yyyymmdd, 4, 2);
     $dd = substr($yyyymmdd, 6, 2);
     $xdate = "$yyyy/$mm/$dd";
   
     $cur = $db->prepare('INSERT OR REPLACE INTO LOGFILE(FILENAME, MTIME) VALUES(:filename, :mtime)');
-    $cur->bindValue(":filename", $logfile, PDO::PARAM_STR);
+    $cur->bindValue(":filename", $logfile_title, PDO::PARAM_STR);
     $cur->bindValue(":mtime", $mtime, PDO::PARAM_INT);
     $cur->execute();
   
@@ -86,33 +136,38 @@ foreach ($logfile_list as $logfile) {
     $cur->execute();
   
     $ncount_dl = 0;
-    $fp = fopen($logfile, "r");
+    //$fp = fopen($logfile, "r");
+    $fp = gzopen($logfile, "r");
     while ($line = fgets($fp)) {
-        if (preg_match($PATTERN, $line, $result)) {
-            $ipaddr = $result[1];
-            $xdate_en = $result[2];
-            $xtime = $result[3];
-            $title = $result[4];
-      
-            if (in_array($title, $IGNORE)) {
-                continue;
-            }
-  
-            $geo = GeoIP_record_by_addr($geoip, $ipaddr);
-            $country = $geo->country_name;
-            $city = $geo->city;
-      
-            $cur = $db->prepare($sql_ins_download);
-            $cur->bindValue(":ipaddr", $ipaddr, PDO::PARAM_STR);
-            $cur->bindValue(":xdate_en", $xdate_en, PDO::PARAM_STR);
-            $cur->bindValue(":xtime", $xtime, PDO::PARAM_STR);
-            $cur->bindValue(":title", $title, PDO::PARAM_STR);
-            $cur->bindValue(":xdate", $xdate, PDO::PARAM_STR);
-            $cur->bindValue(":country", $country, PDO::PARAM_STR);
-            $cur->bindValue(":city", $city, PDO::PARAM_STR);
-            $cur->execute();
+        
+        $result = purse_log($line, $MODE, $BASEDIR);
+        if (count($result) > 0) {
+          
+          
+          $ipaddr = $result[0];
+          $xdate_en = $result[1];
+          $xtime = $result[2];
+          $title = $result[3]; 
+          
+          if (in_array($title, $IGNORE)) {
+              continue;
+          }
 
-            $ncount_dl++;
+          $geo = GeoIP_record_by_addr($geoip, $ipaddr);
+          $country = $geo->country_name;
+          $city = $geo->city;
+    
+          $cur = $db->prepare($sql_ins_download);
+          $cur->bindValue(":ipaddr", $ipaddr, PDO::PARAM_STR);
+          $cur->bindValue(":xdate_en", $xdate_en, PDO::PARAM_STR);
+          $cur->bindValue(":xtime", $xtime, PDO::PARAM_STR);
+          $cur->bindValue(":title", $title, PDO::PARAM_STR);
+          $cur->bindValue(":xdate", $xdate, PDO::PARAM_STR);
+          $cur->bindValue(":country", $country, PDO::PARAM_STR);
+          $cur->bindValue(":city", $city, PDO::PARAM_STR);
+          $cur->execute();
+
+          $ncount_dl++;
         }
     }
     fclose($fp);
